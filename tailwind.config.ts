cc97a6fc3a022dfd1a22bba2983ce2f96d58678b
정희정 <heejj1111@gmail.com>
2026-02-12 17:31:06 +0900

    chore: update tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,20 @@
-import type { Config } from 'tailwindcss';
+import type { Config } from "tailwindcss";
 
 const config: Config = {
-  darkMode: ['class'],
-  content: [
-    './src/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  darkMode: ["class"],
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: [
+          "Pretendard",
+          "-apple-system",
+          "BlinkMacSystemFont",
+          "Segoe UI",
+          "sans-serif",
+        ],
+      },
+    },
   },
   plugins: [],
 };
